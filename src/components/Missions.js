a7a2e30e52cf41a697bb42570e364669c6758fa7
Willian Dutra <contatowilliandutra@gmail.com>
2022-09-21 16:07:00 -0300

    Correções e Requisito 9 - Criando MissionCard

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Title from './Title';
+import MissionCard from './MissionCard';
 
 class Missions extends React.Component {
   render() {
     return (
       <div data-testid="missions">
         <Title headline="Missões" />
+        <MissionCard />
       </div>
     );
   }
