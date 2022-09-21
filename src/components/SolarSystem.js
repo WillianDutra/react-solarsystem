1f3fadd4aaca79a4ced2464694b7e5f524e0c379
Willian Dutra <contatowilliandutra@gmail.com>
2022-09-21 16:35:43 -0300

    Correções ESlint

--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -6,7 +6,7 @@ import planets from '../data/planets';
 class SolarSystem extends React.Component {
   render() {
     return (
-      <div data-testid="solar-system">
+      <div data-testid="solar-system" id="solar-system">
         <Title headline="Planetas" />
         { planets.map((planet, i) => {
           const { name, image } = planet;
